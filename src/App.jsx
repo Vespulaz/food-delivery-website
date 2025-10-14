5c5921b4b48130d16a23d61f4ea59f39fb11db22
Vespulaz <vespulaz.work@gmail.com>
2025-10-14 23:06:13 +0700

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Cart from "./pages/Cart/Cart.jsx";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
-      <div className="app">
-          {/*Navigation bar*/}
-        <Navbar />
-          <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/cart" element={<Cart/>} />
-              <Route path="/order" element={<PlaceOrder/>} />
-          </Routes>
-      </div>
+      <>
+          <div className="app">
+              {/*Navigation bar*/}
+              <Navbar />
+              <Routes>
+                  <Route path="/" element={<Home/>} />
+                  <Route path="/cart" element={<Cart/>} />
+                  <Route path="/order" element={<PlaceOrder/>} />
+              </Routes>
+          </div>
+          <Footer />
+      </>
   )
 }
 
