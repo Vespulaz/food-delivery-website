a9600fade6f333d243984991374a44fc1cfd49c9
Vespulaz <vespulaz.work@gmail.com>
2025-10-15 23:19:10 +0700

    Remove id

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -8,7 +8,7 @@ function FoodDisplay({category}) {
     const {food_list} = useContext(StoreContext);
 
     return (
-        <div className="food-display" id="food-display">
+        <div className="food-display">
             <h2>Top dishes near you</h2>
             <div className="food-display-list">
                 {food_list.map((item, index) => {
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import {assets} from "../../assets/frontend_assets/assets.js";
 
 function Footer() {
     return (
-        <div className="footer" id="footer">
+        <div className="footer">
             <div className="footer-content">
                 <div className="footer-content-left">
                     <img src={assets.logo} alt=""/>
