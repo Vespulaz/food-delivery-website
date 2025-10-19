52aadbfd87322a9868eab7eb0db9ad9ef2d1181b
Vespulaz <vespulaz.work@gmail.com>
2025-10-19 11:18:19 +0700

    Add getTotalCartAmount

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-﻿import React, {createContext, useEffect} from "react";
+﻿import React, {createContext} from "react";
 import {food_list} from "../assets/frontend_assets/assets.js";
 
 
@@ -22,9 +22,16 @@ const StoreContextProvider = (props) => {
         setCartItems(prev => ({...prev, [itemId]: prev[itemId] - 1}));
     }
 
-    useEffect(() => {
-        console.log(cartItems);
-    }, [cartItems]);
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
 
     const contextValue = {
         food_list,
@@ -32,6 +39,7 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
+        getTotalCartAmount,
     }
 
     return (
