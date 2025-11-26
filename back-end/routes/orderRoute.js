c21249ceb17382840b380becc5362558f3d1bfb0
nMintC <nmcuongg1302@gmail.com>
2025-11-27 01:19:35 +0700

    order + cart

---
+++ back-end/routes/orderRoute.js
@@ -0,0 +1,13 @@
+// back-end/routes/orderRoute.js
+import express from "express";
+import auth from "../middleware/authMiddleware.js";
+import { placeOrder, verifyOrder, userOrders, listOrders } from "../controllers/orderController.js";
+
+const orderRoute = express.Router();
+
+orderRoute.post("/place", auth, placeOrder);
+orderRoute.post("/verify", verifyOrder); // có thể để không cần auth tùy flow
+orderRoute.post("/userorders", auth, userOrders); // user order for frontend
+orderRoute.get("/list", listOrders); // listing orders for admin
+
+export default orderRoute;
