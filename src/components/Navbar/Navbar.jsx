d0491cbfabea6431e442f8d5bb674daa16168c52
Vespulaz <vespulaz.work@gmail.com>
2025-12-08 17:23:54 +0700

    Add navbar profile dropdown

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 ﻿import React, {useContext, useEffect} from 'react';
 import './Navbar.css';
 import {assets} from "../../assets/frontend_assets/assets.js";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {StoreContext} from "../../context/StoreContext.jsx";
 
 function Navbar({setShowLogin}) {
@@ -9,11 +9,19 @@ function Navbar({setShowLogin}) {
     // menu stores the currently active menu key.
     // setMenu updates that value when a user clicks a menu item
     const [menu, setMenu] = React.useState("home");
+    const {getTotalCartAmount, token, setToken} = useContext(StoreContext);
 
     const [isScroll, setIsScroll] = React.useState(false);
 
     // Create a reference to the menu's ul tag
     const menuRef = React.useRef(null);
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setToken("");
+        navigate("/");
+    }
 
     // Handle scroll when user scrolls
     useEffect(() => {
@@ -91,8 +99,6 @@ function Navbar({setShowLogin}) {
         }
     }
 
-    const {getTotalCartAmount} = useContext(StoreContext);
-
     return (
         <div className={`navbar ${isScroll ? "scrolled" : ""}`} id="home">
             <div className="navbar-container">
@@ -119,7 +125,17 @@ function Navbar({setShowLogin}) {
                         {/*This dot is shown if have any items in the cart*/}
                         <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
                     </div>
-                    <button onClick={() => setShowLogin(true)}>sign in</button>
+                    {!token
+                        ?<button onClick={() => setShowLogin(true)}>sign in</button>
+                        : <div className="navbar-profile">
+                            <img src={assets.profile_icon} alt=""/>
+                            <ul className="nav-profile-dropdown">
+                                <li onClick={() => navigate("/myorders")}><img src={assets.bag_icon} alt=""/><p>Orders</p></li>
+                                <hr/>
+                                <li onClick={logout}><img src={assets.logout_icon} alt=""/><p>Logout</p></li>
+                            </ul>
+                        </div>}
+
                 </div>
             </div>
         </div>
