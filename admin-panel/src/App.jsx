6cc554a7c06a00b5ab04288f66c2887a8be21f32
Tran Duy Hai Dang <84973401+Vespulaz@users.noreply.github.com>
2025-12-08 18:32:29 +0700

    Change Url variable

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 
 function App() {
 
-    const url = "http://localhost:4000";
+    const url = import.meta.env.VITE_API_BASE_URL;
 
     return (
         <div>
